687da03c7cf49c13cf7527ffedaf51e7ea6f87bc
sallybahzad <sallybahzad2010@gmail.com>
2024-12-18 17:33:44 +0300

    demo

---
+++ lesson 2/demo.py
@@ -0,0 +1,3 @@
+
+text = str(input("Enter a string: "))
+
---
+++ lesson 3/opperators.py
@@ -0,0 +1,12 @@
+tree1 =23
+tree2 =54
+tree3 =67
+tree4 =89
+sum = tree1+tree2+tree3+tree4
+print("the sum of all the 4 trees is: ", sum)
+avarage =sum/4
+print("the avarage of all the 4 trees is: ", avarage)
+
+
+
+
