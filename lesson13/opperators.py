a51477552bb2ea9a34ce9b6d8d511e87f40a45c0
sallybahzad <sallybahzad2010@gmail.com>
2025-02-19 17:51:54 +0300

    demo

---
+++ lesson13/opperators.py
@@ -0,0 +1,28 @@
+def add(P, Q):
+    return P+Q
+def subtract(P, Q):
+    return P-Q
+def multiply(P, Q):
+    return P*Q
+def divide(P, Q):
+    return P/Q
+print("please select opperation")
+print("a. add")
+print("b. subtract")
+print("c. multiply")
+print("d. divide")
+choice= input("please enter the choice (a/b/c/d):")
+num_1= int(input("please enter the first number:"))
+num_2= int(input("please enter the second number:"))
+if choice=='a':
+    print(num_1, "+", num_2, "=", add(num_1, num_2))
+elif choice=='b':
+    print(num_1, "-", num_2, "=", subtract(num_1, num_2))
+elif choice=='c':
+    print(num_1, "*", num_2, "=", multiply(num_1, num_2))
+elif choice=='d':
+    print(num_1, "/", num_2, "=", divide(num_1, num_2))
+else:
+    print("this is an invalid input")
+
+
---
+++ lesson13/return.py
@@ -0,0 +1,4 @@
+r=float(input("enter radius:"))
+def circle_circumfrance(r):
+    return 2*3.14*r
+print(circle_circumfrance(9))
